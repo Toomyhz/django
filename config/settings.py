0c187eb63a6992985d48a8f8ed2cf4f48aaf44e7
ToomyHz <to.hernandezm@duocuc.cl>
2023-05-03 13:22:24 -0400

    Arreglo de estructura

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+from os.path import join
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [join(BASE_DIR),'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
